38345a412b7221bd07a66a9f025f224926177f23
Amimaro <amir.zahlan@gmail.com>
2018-02-21 19:39:35 -0300

    Add stock api urls

--- server/model/stock/controller.js
+++ server/model/stock/controller.js
@@ -1,6 +1,27 @@
 const Controller = require('../../lib/controller');
 const stock = require('./facade');
 
-class Stock extends Controller {}
+const stockApi = 'https://www.alphavantage.co/query?';
+const timeframe = {
+  m1: 'function=TIME_SERIES_INTRADAY&interval=1min&apikey=' + process.env.STOCK_KEY + '&symbol=',
+  m5: 'function=TIME_SERIES_INTRADAY&interval=5min&apikey=' + process.env.STOCK_KEY + '&symbol=',
+  m15: 'function=TIME_SERIES_INTRADAY&interval=15min&apikey=' + process.env.STOCK_KEY + '&symbol=',
+  m30: 'function=TIME_SERIES_INTRADAY&interval=30min&apikey=' + process.env.STOCK_KEY + '&symbol=',
+  h1: 'function=TIME_SERIES_INTRADAY&interval=60min&apikey=' + process.env.STOCK_KEY + '&symbol=',
+  d1: 'function=TIME_SERIES_DAILY&apikey=' + process.env.STOCK_KEY + '&symbol=',
+  w1: 'function=TIME_SERIES_WEEKLY&apikey=' + process.env.STOCK_KEY + '&symbol=',
+  mn: 'function=TIME_SERIES_MONTHLY&apikey=' + process.env.STOCK_KEY + '&symbol='
+};
+
+class Stock extends Controller {
+
+  stockTimeSeries(req, res, next) {
+    console.log('Interval: ' + req.params.interval)
+    console.log('Symbol: ' + req.params.symbol)
+    console.log(stockApi + timeframe[req.params.interval] + req.params.symbol)
+    res.sendStatus(204);
+  }
+
+}
 
 module.exports = new Stock(stock);
--- server/model/stock/router.js
+++ server/model/stock/router.js
@@ -2,13 +2,7 @@ const controller = require('./controller');
 const Router = require('express').Router;
 const router = new Router();
 
-router.route('/')
-  .get((...args) => controller.find(...args))
-  .post((...args) => controller.create(...args));
-
-router.route('/:id')
-  .put((...args) => controller.update(...args))
-  .get((...args) => controller.findById(...args))
-  .delete((...args) => controller.remove(...args));
+router.route('/:interval/:symbol')
+  .get((...args) => controller.stockTimeSeries(...args));
 
 module.exports = router;
